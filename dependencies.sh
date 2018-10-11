71d611492c991ee37a9604bbd8a908e0fb929760
Matt Shinall <matt.shinall@gmail.com>
2018-10-11 11:20:09 -0400

    updated

--- dependencies.sh
+++ dependencies.sh
@@ -1,12 +1,29 @@
 #!/usr/bin/bash
+
+sudo apt-get install \
+	rtl-sdr \
+	sox \
+	alsa \
+	cmake \
+	libusb
+
 pip install \
 	sys \
 	pad4pi \
 	time \
-	subprocess
+	subprocess \
+	os \
+	signal \
+	shlex
 
 #git clone https://github.com/osmocom/rtl-sdr.git
-sudo apt-get install \
-	rtl-sdr \
-	sox \
-	alsa
+#cd rtl-sdr
+#cmake ../
+#make
+#sudo make install
+
+git clone https://github.com/sysrun/rtl-sdr.git
+cd rtl-sdr
+cmake ../
+make
+sudo make install
